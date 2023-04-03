c6747dc412aa41c3359e6686d4f0a8f24082369e
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2023-04-03 23:38:50 +0200

    Add project files.

---
+++ WpfApp1/BookTypeToBackgroundConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using System;
+
+namespace WpfApp1
+{
+    public class BookTypeToBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || !(value is string) || String.Equals("", value)) return Brushes.Transparent;
+
+            string type = (string)value;
+            if (type.Equals("PaperBack", StringComparison.OrdinalIgnoreCase))
+            {
+                return Brushes.LightGray;
+            }
+            else
+            {
+                return Brushes.Transparent;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
